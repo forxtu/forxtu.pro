d6f4efe95d9c5cee0a893c7d18a89cffc54123b1
forxtu <forxtu@gmail.com>
2017-07-28 16:25:40 +0200

    feat(gulpfile): add browser sync; refactor(gulpfile): small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,25 @@
 var gulp = require("gulp");
 
 // Include Our Plugins
-var jshint = require("gulp-jshint");
-var sass = require("gulp-sass");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
-var moreCSS = require("gulp-more-css");
-//var cssnano = require("gulp-cssnano");
-var jade = require("gulp-jade");
+var jshint = require("gulp-jshint"),
+    sass = require("gulp-sass"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    moreCSS = require("gulp-more-css"),
+    //cssnano = require("gulp-cssnano"),
+    //jade = require("gulp-jade"),
+    browserSync = require('browser-sync');
+
+
+// Browser Sync
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+  })
+});
 
 // Lint Task
 gulp.task("lint", function() {
@@ -22,8 +33,11 @@ gulp.task("lint", function() {
 gulp.task("sass", function() {
   return gulp.src("src/scss/*.scss")
     .pipe(sass())
-//    .pipe(cssnano())
-    .pipe(gulp.dest("dist/css"));
+   // .pipe(cssnano())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Concatenate & Minify JS
@@ -31,25 +45,37 @@ gulp.task("scripts", function() {
   return gulp.src("src/js/*.js")
     .pipe(rename("app.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest("dist/js"))
+    //browser sync
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task("jade", function() {
-  var locals = new Object();
+// gulp.task("jade", function() {
+//   var locals = new Object();
 
-  gulp.src("src/*.jade")
-    .pipe(jade({
-      locals: locals
-    }))
-    .pipe(gulp.dest("dist/"))
-});
+//   gulp.src("src/*.jade")
+//     .pipe(jade({
+//       locals: locals
+//     }))
+//     .pipe(gulp.dest("dist/"))
+// });
 
 // Watch Files For Changes
-gulp.task("watch", function() {
+gulp.task("watch", ["browserSync", "sass", "scripts"], function() {
+  // Watch .js files
   gulp.watch("src/js/*.js", ["lint", "scripts"]);
+
+  // Watch .scss files
   gulp.watch("src/scss/*.scss", ["sass"]);
-  gulp.watch("src/*.jade", ["jade"]);
+
+  // Watch .jade files
+  // gulp.watch("src/*.jade", ["jade"]);
+
+  // Watch .html files
+  gulp.watch("dist/*.html", browserSync.reload); 
 });
 
 // Default Task
-gulp.task("default", ["lint", "sass", "scripts", "jade", "watch"]);
+gulp.task("default", ["lint", "sass", "scripts", "watch"]);  //add "jade" if need
